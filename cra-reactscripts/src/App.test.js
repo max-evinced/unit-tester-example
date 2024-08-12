2007c15b53f458491e6a356580dd3d7329cee342
Max Dobeck <max.dobeck@evinced.com>
2024-08-12 12:17:33 -0400

    test

---
+++ cra-reactscripts/src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import EvincedUT, { configure } from '@evinced/unit-tester';
+
+configure({
+  serviceAccountId: process.env.SERVICE_ACCOUNT_ID,
+  serviceAccountSecret: process.env.API_KEY,
+  suppressAnonymousAuthenticationWarning: true
+})
+
+test('renders learn react link', async () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  const results = await EvincedUT.analyzeLink(linkElement);
+  console.log("results: ", results);
+});
